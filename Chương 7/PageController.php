f94a13e03dd264b9e3128c670490c3001047aab7
Nanhbip <namanhga2005@gmail.com>
2025-12-22 10:20:30 +0700

    Nộp bài chương 7

---
+++ Chương 7/PageController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    // Sửa phương thức showHomepage() của Chương 6
+    public function showHomepage()
+    {
+        $viewTitle = 'PHT Chương 7 - Blade Template';
+        $pageTitle = 'Chào mừng bạn đến với Blade!';
+        $pageDescription = 'Đây là trang chủ được render bằng Blade Template Engine.';
+
+        $tasks = [
+            'Cài đặt Laravel',
+            'Hiểu về Routing & Controller',
+            'Tạo Layout với Blade',
+            'Truyền dữ liệu cho View'
+        ];
+
+        // Trả về View + truyền dữ liệu
+        return view('homepage', [
+            'title' => $viewTitle,
+            'page_title' => $pageTitle,
+            'page_description' => $pageDescription,
+            'tasks' => $tasks
+        ]);
+    }
+}
